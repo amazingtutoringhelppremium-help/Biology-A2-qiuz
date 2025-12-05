2f8968a7c54cb63e9b685d30060eda22d47c4e31
atfam <131915994+atfam@users.noreply.github.com>
2025-12-04 18:26:40 -0600

    Delete SaveGen directory

--- SaveGen/Helper.js
+++
@@ -1,7 +0,0 @@
-// Get redirect parameter from URL
-const urlParams = new URLSearchParams(window.location.search);
-const redirectTo = urlParams.get('redirect') || '/';
-
-setTimeout(() => {
-    window.location.href = redirectTo;
-}, 15000);
